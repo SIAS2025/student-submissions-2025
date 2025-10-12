10423813b34179525e41f5b87fdecd94d83fedfc
llwang <llwang0124@163.com>
2025-10-12 11:27:45 +0800

    test

--- WangLulu/test.java
+++ WangLulu/test.java
@@ -1,3 +1,7 @@
 public class test {
-
+    // main方法：Java程序的入口，程序从这里开始执行
+    public static void main(String[] args) {
+        // 打印"Hello World"（注意修正拼写："hell哦world"应为"Hello World"，符合通用表述）
+        System.out.println("Hello World");
+    }
 }
