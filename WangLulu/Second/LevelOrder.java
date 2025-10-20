5bc06e1ee11aa88fd217d69e091aeebfd4290756
llwang <llwang0124@163.com>
2025-10-20 14:43:37 +0800

    update wll

---
+++ WangLulu/Second/LevelOrder.java
@@ -0,0 +1,36 @@
+package Second;
+
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrder {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if(root != null){
+            queue.add(root);
+        }
+        while(!queue.isEmpty()){
+            int n = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if(node.left !=null ){
+                    queue.add(node.left);
+                }
+                if(node.right != null){
+                    queue.add(node.right);
+                }
+            }
+            result.add(level);
+        }
+
+
+        return result;
+    }
+
+}
